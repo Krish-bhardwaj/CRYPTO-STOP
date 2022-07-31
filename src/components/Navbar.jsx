a8befca3cbd9d8c80b42e5c21b5e95fac35ffb36
Krish-bhardwaj <krishbhardwaj27203@gmail.com>
2022-07-31 12:33:18 +0530

    final

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Menu, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Typography.Title level={2} className="logo"><Link to="/">CRYPTO STOP.</Link></Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+      </div>
+      {activeMenu && (
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
